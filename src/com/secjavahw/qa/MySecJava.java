6eb9d1af6a54bd7c7c1bb5ffccb6fa5b95171711
Md Rahman <mdrahman@Mds-MacBook-Pro.local>
2020-01-04 17:59:43 -0500

    adding git file

---
+++ src/com/secjavahw/qa/MySecJava.java
@@ -0,0 +1,54 @@
+package com.secjavahw.qa;
+
+public class MySecJava {
+    public static void main (String [] agrs){
+        firstmethod();
+        secmethord();
+        thirdmethod();
+        fourthmethod();
+        fifthmethod();
+
+    }
+    public static void firstmethod(){
+        int myblance = 200;
+                System.out.println(myblance);
+                String myFirstJavaHw = "i love bangladesh";
+                System.out.println(myFirstJavaHw);
+
+}
+
+    public static void secmethord(){
+        int mynumber =100;
+                System.out.println(mynumber);
+                String mySecJavaHw = "i love michigan";
+                System.out.println (mySecJavaHw);
+                System.out.println (mySecJavaHw + " " + mynumber);
+    }
+
+    public static void thirdmethod(){
+        int mytotalblance = 300;
+                System.out.println(mytotalblance);
+                String myThirdJavaHw = "i love my family";
+                System.out.println (myThirdJavaHw);
+                System.out.println (mytotalblance + " " + myThirdJavaHw);
+
+
+    }
+
+    public static void fourthmethod(){
+        int mytotalamount = 400;
+                System.out.println(mytotalamount);
+        String myFourthJavaHw = "i love nyc";
+        System.out.println(myFourthJavaHw);
+    }
+
+    public  static void fifthmethod(){
+        int myfinalblance = 500;
+        int myblance = 400;
+                System.out.println(myfinalblance);
+        String myFifthJavaHw = "i love sylhet";
+        System.out.println(myFifthJavaHw);
+        System.out.println(myfinalblance + " " + myblance);
+
+    }
+}
